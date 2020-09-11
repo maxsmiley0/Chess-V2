f2bbd6c764785a0654bd45c8a041e9ecc5f85b1a
Max Smiley <maxsmiley0@gmail.com>
2020-09-11 09:18:47 -0700

    Implemented arrays for files and ranks

--- Chess/defs.js
+++ Chess/defs.js
@@ -4,8 +4,6 @@ EMPTY : 0, wP: 1, wN: 2, wB: 3, wR: 4,  wQ: 5,  wK: 6,
 		   bP: 7, bN: 8, bB: 9, bR: 10, bQ: 11, bK: 12
 };
 
-const BRD_SQ_NUM = 120;
-
 const FILES =
 {
 FILE_A: 0, FILE_B: 1, FILE_C: 2, FILE_D: 3, FILE_E: 4, FILE_F: 5,
@@ -33,4 +31,28 @@ NO_SQ: 99, OFFBOARD: 100
 const BOOL = 
 {
 TRUE: 1, FALSE: 0
+}
+
+/*
+Why are we using an array of 120 (12 x 10) as opposed to an array of 64 (8 x 8)? 
+This simplifies legal move generation down the line; for example, we will know when to
+stop checking if "sliding" pieces can move in a given direction. 
+
+Why do we (12 x 10) as opposed to (10 x 10)? This is so knights on the 1st or 8th rank
+can move two squares up / down and not access an undefined element. 
+
+Why don't we need it to be (12 x 12) as opposed to (12 x 10) following this logic? 
+The board only needs one side of padding on its left and right, since due to the numbering
+system of the board, if the knight is on the first or last file and tries to move two
+spaces in the left or right direction, it will always land on a well-defined square.
+*/
+const BRD_SQ_NUM = 120;
+
+const FilesBrd = new Array(BRD_SQ_NUM);
+const RanksBrd = new Array(BRD_SQ_NUM);
+
+//Converts a file and rank to number
+function FR2SQ (f, r)
+{
+	return (21 + f) + (10 * r);
 }
--- Chess/main.js
+++ Chess/main.js
@@ -2,10 +2,30 @@ $(function()
 {
 	init();
 	console.log("Main Init Called");
-	console.log(FILES.FILE_NONE);
 });
+
+function InitFileRanksBrd()
+{	
+	//Filling entire array with the OFFBOARD value
+	for (let i = 0; i < BRD_SQ_NUM; i++)
+	{
+		FilesBrd[i] = SQUARES.OFFBOARD;
+		RanksBrd[i] = SQUARES.OFFBOARD;
+	}
+	
+	//Filling the middle 8 x 8 grid with the appropriate numbers
+	for (let rank = RANKS.RANK_1; rank < RANKS.RANK_NONE; rank++)
+	{
+		for (let file = FILES.FILE_A; file < FILES.FILE_NONE; file++)
+		{
+			FilesBrd[FR2SQ(file, rank)] = file;
+			RanksBrd[FR2SQ(file, rank)] = rank;
+		}
+	}
+}
 
 function init()
 {
 	console.log("init() called");
+	InitFileRanksBrd();
 }

cb0e85572d995c97424b6acb25357a70fa30c83e
Max Smiley <maxsmiley0@gmail.com>
2020-09-11 10:04:12 -0700

    Implemented piece lists
    Engine only has to loop over eact number of pieces to generate moves, as opposed to every square

--- Chess/board.js
+++ Chess/board.js
@@ -1,3 +1,9 @@
+//Maps a given piece to its "maximum index". Takes in parameter piece name e.g. wP
+function PCEINDEX(pce)
+{
+	return (pce * 10 + GameBoard.pceNum(pce));
+}
+
 var GameBoard = 
 {
 	pieces: new Array(BRD_SQ_NUM),	//pieces in a given game board
@@ -5,5 +11,24 @@ var GameBoard =
 	fiftyMove: 0,					//50 move draw rule
 	hisPly: 0,						//Stack of game states / moves
 	ply: 0,							//Number of plies into the search tree	
-	castlePerm: 0					//Can a given side castle?		
+	castlePerm: 0,					//Can a given side castle?		
+	material: new Array(2),			//Indexed by side, holds value of material
+	pceNum: new Array(13),			//Indexed by piece, how many of a given piece exists?
+	
+	/*
+	pList stores position of each pieces
+	How can one piece have an index? Especially if there are multiple of them?
+	We use the value of that piece * 10 + number of pieces left on the board of that type
+	E.g. white knight has a value of wN = 2, lets say there are 7 white knights on the 
+	board (some sort of crazy promotion scheme)
+	Then the knights will correspond to wN * 10 + GameBoard.pceNum(wN)
+	= 2 * 10 + GameBoard.pceNum(2) = 20, 21, 22, 23, 24, 25, 26
+	And these indicies in PlistArray will correspond to an integer representing the positions
+	of the white knights
+	Why 10? No board can have more than 10 pieces, so the indicies will never overlap
+	*/
+	
+	//Making sure pList has enough space
+	pList: new Array(14 * 10)
+	
 };
